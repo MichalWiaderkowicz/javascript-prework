555221b08194191dc75f4fa0f2e594d25439c237
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2023-10-04 10:02:21 +0200

    code upgrade

--- js/script.js
+++ js/script.js
@@ -16,11 +16,11 @@
                  return 'nieznany ruch';
                }
           }
-          let randomNumber = Math.floor(Math.random() * 3 + 1);
+          const randomNumber = Math.floor(Math.random() * 3 + 1);
           
           console.log('Wylosowana liczba to: ' + randomNumber);
           
-          let computerMove = getMoveName(randomNumber);
+          const computerMove = getMoveName(randomNumber);
           /*
           if(randomNumber == 1){
                computerMove = 'kamień';
@@ -38,7 +38,7 @@
           
           console.log('Gracz wpisał: ' + playerInput);
           
-          let playerMove = getMoveName(playerInput);
+          const playerMove = getMoveName(playerInput);
           /*
           if(playerInput == '1'){
                playerMove = 'kamień';
